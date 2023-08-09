0271de82e0fc63864e5d9c8f5df0b2fa606ef43f
oleg sukharev <olegsukharev1991@gmail.com>
2023-08-09 21:57:02 +0200

    env setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,25 @@
+import { Nunito } from "next/font/google";
+
 import './globals.css'
 
+export const metadata = {
+    title: "Airbnb",
+    description: "Airbnb clone"
+}
+
+const font = Nunito({
+    subsets: ["latin"]
+})
+
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <head />
+            <body className={font.className}>{children}</body>
+        </html>
+    )
 }
