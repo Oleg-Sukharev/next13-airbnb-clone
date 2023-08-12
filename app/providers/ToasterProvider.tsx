6e969ae4d5bccaf042eaf1c78215141c0431ff72
oleg sukharev <olegsukharev1991@gmail.com>
2023-08-12 22:38:14 +0200

    ToasterProvider

---
+++ app/providers/ToasterProvider.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { Toaster } from "react-hot-toast";
+// create component to make sure it use client directive 
+const ToasterProvider = () => {
+  return (
+    <Toaster />
+  );
+}
+
+export default ToasterProvider;
