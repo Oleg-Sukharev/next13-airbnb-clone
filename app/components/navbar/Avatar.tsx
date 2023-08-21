fdbe2a17af164423b76e63f6181f5ccccb6e2f65
oleg sukharev <olegsukharev1991@gmail.com>
2023-08-22 00:09:24 +0200

    Social Login (Google and Github)

--- app/components/navbar/Avatar.tsx
+++ app/components/navbar/Avatar.tsx
@@ -1,7 +1,12 @@
 "use client";
 import Image from "next/image";
 
-const Avatar = () => {
+interface AvatarProps {
+  src: string | null | undefined
+}
+const Avatar: React.FC<AvatarProps> = ({
+  src
+}) => {
   return (
     <div>
       <Image
@@ -9,7 +14,7 @@ const Avatar = () => {
         height="30"
         width="30"
         alt="Avatar"
-        src={'/images/placeholder.jpg'}
+        src={src || '/images/placeholder.jpg'}
       />
     </div>
   )
